9186fd11c58fce8dd7f30353d5bebb7a0e327964
skarud2 <lnk7576@gmail.com>
2025-05-15 12:37:03 +0900

    Remove all dir except nk

--- yuno/week3/my-app/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportTrailingSlash: true,
-  output: "export",
-  basePath: "/13th_FRONT-END_ASSIGNMENT/yuno/week3/my-app/out",
-}
-
-export default nextConfig
